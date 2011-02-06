66b305e8b2e74385b66e9f0f6c20c263fbcdc38e
kuno <neokuno@gmail.com>
2011-02-06 12:01:40 +0800

    added stage.sh

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ utils
 
 * wma2mp3.sh -- convert wma to mp3
 
-* convert.py convert text to utf8
+* convert.py -- convert text to utf8
+
+* stage.sh   -- report my own status
---
+++ stage.sh
@@ -0,0 +1,10 @@
+#~/usr/bin/env sh
+
+sp=$1
+dp=$2
+p=$3
+w=$4
+
+curl --data-urlencode "" "http://127.2.0.3:8003/api?sp=$sp&dp=$dp&p=$p&w=$w"
+
+curl --data-urlencode "" "http://statusongae.appspot.com/api?sp=$sp&dp=$dp&p=$p&w=$w" 
